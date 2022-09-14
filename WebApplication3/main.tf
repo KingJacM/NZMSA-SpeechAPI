05fdf7c1b9ebaacd6ecd654c81f5ddf469b899ab
KingJac <924756831@qq.com>
2022-09-14 14:28:16 +1200

    Update main.tf

--- WebApplication3/main.tf
+++ WebApplication3/main.tf
@@ -2,9 +2,10 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source  = "registry.terraform.io/hashicorp/azurerm"
       version = "~> 3.0.2"
     }
+    
   }
 
   required_version = ">= 1.1.0"
@@ -15,6 +16,28 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
+  name     = "TerraformSpeechAPI"
   location = "Australia East"
+}
+
+resource "azurerm_container_group" "cg" {
+  name                      = "speechapi"
+  location                  = azurerm_resource_group.rg.location
+  resource_group_name       = azurerm_resource_group.rg.name
+
+  ip_address_type     = "Public"
+  dns_name_label      = "kingjactfapi"
+  os_type             = "Linux"
+
+  container {
+      name            = "speechapi"
+      image           = "kingjac/speechapi"
+        cpu             = "1"
+        memory          = "1"
+
+        ports {
+            port        = 80
+            protocol    = "TCP"
+        }
+  }
 }
